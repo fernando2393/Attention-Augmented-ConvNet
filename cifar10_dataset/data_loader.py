bf051b37f3c57891ad3c0c7bebc4d01f89e27edd
fernando2393 <garciasanzfernando2393@gmail.com>
2020-05-04 21:18:54 +0200

    Code cleaned. Plotting and preprocessing functionalities enhanced.

--- cifar10_dataset/data_loader.py
+++ cifar10_dataset/data_loader.py
@@ -2,16 +2,15 @@
 """
 Created on Sun May  3 12:52:00 2020
 
-@author: flaviagv
+@author: MatteoDM, FernandoGS, FlaviaGV
 """
 
-
-import numpy as np 
+import numpy as np
 from tensorflow.keras.datasets import cifar10
 import tensorflow.keras
 
 
-def preprocess_features(x_train, x_test, substract_pixel_mean):
+def preprocess_features(x_train, x_test, substract_pixel_mean=True):
     """
     Perform normalization (all values between 0 and 1) and mean normalization
     if substract_pixel_mean=True.
@@ -28,21 +27,22 @@ def preprocess_features(x_train, x_test, substract_pixel_mean):
     x_test : numpy array
 
     """
-    
+
     x_train = x_train.astype('float32') / 255
     x_test = x_test.astype('float32') / 255
-    
+
     if substract_pixel_mean:
         x_train_mean = np.mean(x_train, axis=0)
         x_train -= x_train_mean
         x_test -= x_train_mean
-    
-    return x_train, x_test 
-    
 
-    
-def get_train_val_test_data(n_val_samples, categorical_targets=True, 
-                            substract_pixel_mean=True, verbose=0):
+        return x_train, x_test, x_train_mean
+
+    return x_train, x_test
+
+
+def get_train_val_test_data(n_val_samples, categorical_targets=True,
+                            substract_pixel_mean=True, verbose=False):
     """
     CIFAR-10 ready for training, validating and testing the model/s. 
     
@@ -59,31 +59,37 @@ def get_train_val_test_data(n_val_samples, categorical_targets=True,
 
     """
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    x_train, x_test = preprocess_features(x_train, x_test, substract_pixel_mean)
-    
+    x_train_mean = 0
+
+    if substract_pixel_mean:
+        x_train, x_test, x_train_mean = preprocess_features(x_train, x_test, substract_pixel_mean)
+    else:
+        x_train, x_test = preprocess_features(x_train, x_test, substract_pixel_mean)
+
     num_classes = len(np.unique(y_train))
-    
+
     (x_train, y_train), (x_val, y_val) = split_train_dataset(x_train, y_train, n_val_samples)
-    
+
     if categorical_targets:
         y_train = tensorflow.keras.utils.to_categorical(y_train, num_classes)
         y_val = tensorflow.keras.utils.to_categorical(y_val, num_classes)
         y_test = tensorflow.keras.utils.to_categorical(y_test, num_classes)
 
-    
     if verbose:
-        print("Shape x_train: ", x_train.shape, "\n", 
-              "Shape y_train: ", y_train.shape, "\n", 
-              "Shape x_validation: ", x_val.shape, "\n", 
+        print("Shape x_train: ", x_train.shape, "\n",
+              "Shape y_train: ", y_train.shape, "\n",
+              "Shape x_validation: ", x_val.shape, "\n",
               "Shape y_validation: ", y_val.shape, "\n",
-              "Shape x_test: ", x_test.shape, "\n", 
+              "Shape x_test: ", x_test.shape, "\n",
               "Shape y_test: ", y_train.shape, "\n")
-    
-    
+
+    if substract_pixel_mean:
+        return (x_train, y_train), (x_val, y_val), (x_test, y_test), x_train_mean
+
     return (x_train, y_train), (x_val, y_val), (x_test, y_test)
 
 
-def split_train_dataset(x_train, y_train, n_val_samples):  
+def split_train_dataset(x_train, y_train, n_val_samples):
     """
     Parameters
     ----------
@@ -99,6 +105,6 @@ def split_train_dataset(x_train, y_train, n_val_samples):
     """
     indices = np.random.permutation(x_train.shape[0])
     val_idx, train_idx = indices[:n_val_samples], indices[n_val_samples:]
-    x_train, x_val = x_train[train_idx,:], x_train[val_idx,:]
+    x_train, x_val = x_train[train_idx, :], x_train[val_idx, :]
     y_train, y_val = y_train[train_idx], y_train[val_idx]
-    return (x_train, y_train), (x_val, y_val)    
+    return (x_train, y_train), (x_val, y_val)
--- cifar10_dataset/utils.py
+++ cifar10_dataset/utils.py
@@ -2,20 +2,27 @@
 """
 Created on Sun May  3 13:58:02 2020
 
-@author: Matosevic
+@author: MatteoDM, FernandoGS, FlaviaGV
 """
 
 import matplotlib.pyplot as plt
-import numpy as np 
+import numpy as np
 
 import keras.layers
 
-def plot_dataset_examples(x_train, n_rows=10):
-  x = x_train.astype("uint8")
 
-  fig, axes1 = plt.subplots(n_rows, n_rows,figsize=(n_rows,n_rows))
-  for j in range(n_rows):
-      for k in range(n_rows):
-          i = np.random.choice(range(len(x)))
-          axes1[j][k].set_axis_off()
-          axes1[j][k].imshow(x[i:i+1][0])
+def plot_dataset_examples(x_train, mean=None, n_rows=10):
+    x = x_train
+
+    if mean is not None:
+        x += mean
+        x *= 255
+
+    x = x.astype("uint8")
+    fig, axes1 = plt.subplots(n_rows, n_rows, figsize=(n_rows, n_rows))
+    for j in range(n_rows):
+        for k in range(n_rows):
+            i = np.random.choice(range(len(x)))
+            axes1[j][k].set_axis_off()
+            axes1[j][k].imshow(x[i:i+1][0])
+    plt.show()
