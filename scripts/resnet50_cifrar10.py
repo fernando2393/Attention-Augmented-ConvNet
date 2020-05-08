21506ac2a2cc2fa31c8385ee2db197e8f010331a
matteo <matteo.demartis.b51s@gmail.com>
2020-05-08 19:34:39 +0200

    script to train resnet50 with data augmentation (to rearrange)

--- scripts/resnet50_cifrar10.py
+++ scripts/resnet50_cifrar10.py
@@ -1,153 +1,153 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon May  4 00:34:40 2020
+Created on Thu May  7 22:19:21 2020
 
 @author: matte
 """
+
 import sys
 sys.path.append("..")
 
 import tensorflow.keras
-from tensorflow.keras.callbacks import LearningRateScheduler
 from models.resnet50 import resnet50_v1
 from cifar10_dataset.data_loader import get_train_val_test_data
 import numpy as np
-import sklearn
 from preprocessing.augmentation import Augment2D
-from tqdm import tqdm
 import tensorflow as tf
 
-
+validation_size = 5000
 batch_size = 128 
 epochs = 500
 num_classes = 10
-validation_size = 5000
-curr_epoch = 0
 
-__cos_lr_passed_epochs = 0 
-__cos_lr_cycles = 0
+class MyLrSchedule():
+    def __init__(self, cos_lr_n_min=0.004, cos_lr_n_max=0.020, cos_lr_T_0=10, cos_lr_T_mult=2, lr_linear_final_epoch=25, batch_size=128):
+        self.cos_lr_n_min = cos_lr_n_min
+        self.cos_lr_n_max = cos_lr_n_max
+        self.cos_lr_T_0 = cos_lr_T_0
+        self.cos_lr_T_mult = cos_lr_T_mult
+        self.lr_linear_final_epoch = lr_linear_final_epoch
+        self.batch_size = batch_size
 
-cos_lr_n_min = 0.004
-cos_lr_n_max = 0.020
-cos_lr_T_0 = 10
-cos_lr_T_mult = 2
-lr_linear_final_epoch = 25
+        self.last_lr = 0.0
 
-def lr_schedule(epoch):
-    """Learning Rate Schedule
-
-    # Arguments
-        epoch (int): The number of epochs
-
-    # Returns
-        lr (float32): learning rate
-    """
-    global __cos_lr_cycles
-    global __cos_lr_passed_epochs
-    
-    if curr_epoch <= lr_linear_final_epoch:
-      lr = 0.2 * batch_size / 256 * (curr_epoch+1) / 25
-      global __cos_lr_passed_epochs
-      __cos_lr_passed_epochs = curr_epoch + 1
-      
-    #lr = 1e-3
-    else:
-        T_curr = curr_epoch - __cos_lr_passed_epochs
-        T_i = cos_lr_T_mult**(__cos_lr_cycles) * cos_lr_T_0
+        self.__cos_lr_passed_epochs = 0 
+        self.__cos_lr_cycles = 0
+  
+    def get_learning_rate(self, epoch):
+      if epoch <= self.lr_linear_final_epoch:
+        lr = 0.2 * self.batch_size / 256 * (epoch + 1) / 25
+        self.__cos_lr_passed_epochs = epoch + 1
+      else:
+        T_curr = epoch - self.__cos_lr_passed_epochs
+        T_i = self.cos_lr_T_mult**(self.__cos_lr_cycles) * self.cos_lr_T_0
         if T_curr == T_i:
-            __cos_lr_cycles += 1
-            __cos_lr_passed_epochs = curr_epoch + 1
+            self.__cos_lr_cycles += 1
+            self.__cos_lr_passed_epochs = epoch + 1
             
-        lr = cos_lr_n_min + 0.5 * (cos_lr_n_max - cos_lr_n_min) * (1 + np.cos(T_curr/(T_i) * np.pi))
-        print('Epoch ', curr_epoch, 'Learning rate: ', lr, 'T_curr ', T_curr, 'T_i ', T_i)
-    return lr
+        lr = self.cos_lr_n_min + 0.5 * (self.cos_lr_n_max - self.cos_lr_n_min) * (1 + np.cos(T_curr/(T_i) * np.pi))
+      return lr
 
+@tf.function
+def train_step(images, labels, model, loss_object, optimizer, train_loss, train_accuracy):
+  with tf.GradientTape() as tape:
+    # training=True is only needed if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    predictions = model(images, training=True)
+    loss = loss_object(labels, predictions)
+  gradients = tape.gradient(loss, model.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-def train_network(x_train, y_train, batch_size=100, epochs=20, validation_data=(), shuffle=True, data_augmentation=False):
-    
-    input_shape = x_train.shape[1:]
-    
-    n_colors = x_train.shape[3]
+  train_loss(loss)
+  train_accuracy(labels, predictions)
+  
+@tf.function
+def test_step(images, labels, model, loss_object, test_loss, test_accuracy):
+  # training=False is only needed if there are layers with different
+  # behavior during training versus inference (e.g. Dropout).
+  predictions = model(images, training=False)
+  t_loss = loss_object(labels, predictions)
 
-    depth = n_colors * 6 + 2
-    
-    model = resnet50_v1(input_shape=input_shape, depth=depth)
-    
-    opt = tensorflow.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=False)
+  test_loss(t_loss)
+  test_accuracy(labels, predictions)
 
-    model.compile(loss='categorical_crossentropy',
-              optimizer=opt,
-              metrics=['accuracy'])
+def train_network(train_data, validation_data, model,  batch_size=100, epochs=20, shuffle=True, data_augmentation=False):
     
-    lr_scheduler = LearningRateScheduler(lr_schedule)
+    lr_scheduler = MyLrSchedule(lr_linear_final_epoch=25)
+
+    loss_object = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+
+    optimizer = tf.keras.optimizers.SGD(learning_rate=lr_scheduler.get_learning_rate(0))
     
-    input_dim = x_train.shape[0]
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
     
-    global curr_epoch
-    curr_epoch = 0
+    train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='train_accuracy')
+
+    test_loss = tf.keras.metrics.Mean(name='test_loss')
+
+    test_accuracy = tf.keras.metrics.CategoricalAccuracy(name='test_accuracy')
+
+    data_augmentation_engine = Augment2D(True, True, True, zero_padding_shape=(40, 40), random_crop_shape=(32, 32))
     
-    for epoch in tqdm(range(epochs)):
-        curr_epoch = epoch
+    for epoch in range(epochs):
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+        test_loss.reset_states()
+        test_accuracy.reset_states()
+        
+        optimizer.lr.assign(lr_scheduler.get_learning_rate(epoch))
+
         if shuffle:
-            x_train, y_train = sklearn.utils.shuffle(x_train, y_train)
-            for batch_idx in range(batch_size):
-                j_start = batch_idx* batch_size
-                j_end = (batch_idx + 1) * batch_size
-                if batch_idx == batch_size - 1:
-                    j_end = input_dim
-                x_batch = x_train[j_start:j_end, :]
-                y_batch = y_train[j_start:j_end, :]
-                
-                if data_augmentation:
-                    data_augmentor = Augment2D(True, True, True, zero_padding_shape=(40, 40), random_crop_shape=(32, 32))
-                    tensor_x_batch = tf.data.Dataset.from_tensor_slices(x_batch)
-                    x_batch = data_augmentor.transform(tensor_x_batch)
-                model.fit(x_batch, y_batch,
-                                  batch_size=batch_size,
-                                  epochs=1,
-                                  validation_data=(validation_data[0], validation_data[1]),
-                                  shuffle=False,
-                                  callbacks=[lr_scheduler], verbose=0)
-    return model
+            epoch_train_data = train_data.shuffle(len(list(train_data)))
+        else:
+            epoch_train_data = train_data
+        if data_augmentation:
+            epoch_train_data = data_augmentation_engine.transform(epoch_train_data)
+            
+        batched_train_data = epoch_train_data.batch(batch_size)
+        for batch_x, batch_y in batched_train_data:
+            #x_batch, y_batch = batch
+            train_step(batch_x, batch_y, model, loss_object, optimizer, train_loss, train_accuracy)
+        '''
+        val_len = len(list(validation_data))
+        x_val, y_val = next(iter(validation_data.batch(val_len)))
+        test_step(x_val, y_val, model, loss_object, test_loss, )
+        '''
+        batched_val_data = epoch_train_data.batch(batch_size)
+        for batch in batched_val_data:
+            test_step(x_val, y_val, model, loss_object, test_loss, test_accuracy)
+        
+        template = 'Epoch {}, Loss: {}, Accuracy: {}, Validation Loss: {}, Validation Accuracy: {}'
+        print(template.format(epoch + 1,
+                              train_loss.result(),
+                              train_accuracy.result() * 100,
+                              test_loss.result(),
+                              test_accuracy.result() * 100))
+
 
 if __name__ == "__main__":
+        
+    (x_train, y_train), (x_val, y_val), (x_test, y_test), _ = get_train_val_test_data(validation_size)
+    print("Data loaded.")
     
-    for e in range(epochs):
-        curr_epoch = e 
-        lr_schedule(e)
-    (x_train, y_train), (x_val, y_val), (x_test, y_test) = get_train_val_test_data(validation_size)
-    '''
+    train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    validation_data = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+    test_data = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+    
     input_shape = x_train.shape[1:]
-    
-    ##TODO: Add data augmentation
-    
     
     n_colors = x_train.shape[3]
 
     depth = n_colors * 6 + 2
     
     model = resnet50_v1(input_shape=input_shape, depth=depth)
-    
-    opt = tensorflow.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=False)
 
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=opt,
-                  metrics=['accuracy'])
-    
-    lr_scheduler = LearningRateScheduler(lr_schedule)
-
-    model.fit(x_train, y_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              validation_data=(x_test, y_test),
-              shuffle=True,
-              callbacks=[lr_scheduler])
-    '''
-    # model = train_network(x_train, y_train,
-    #                       batch_size=batch_size,
-    #                       epochs=epochs,
-    #                       validation_data=(x_val, y_val),
-    #                       data_augmentation=True)
+    train_network(train_data,
+                          validation_data,
+                          model,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          data_augmentation=True)
     
     # scores = model.evaluate(x_test, y_test, verbose=1)
     # print('Test loss:', scores[0])
